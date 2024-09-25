35b63f560aac2070bef662080f3c0bb3e3777ece
john665paes <johnatanpaes@gmail.com>
2024-09-25 16:15:37 -0300

    erro, tentando identificar

--- licensync/App.tsx
+++ licensync/App.tsx
@@ -1,9 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import LoginScreen from './src/screens/login';
+import {Loginscreen} from './src/screens/login/';
 
 export default function App() {
-  return (<LoginScreen/>
+  return (<Loginscreen/>
   );
 }
 
--- licensync/src/screens/login/index.tsx
+++ licensync/src/screens/login/index.tsx
@@ -1,13 +1,18 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
-
+import { View, Text, ImageBackground } from 'react-native';
 export interface LoginscreenProps {
 }
 
-export function App (props: LoginscreenProps) {
-    return (
+export function Loginscreen (props: LoginscreenProps) {
+    return (<ImageBackground source={require('././src/assets/imgs/backlogin.png')}> 
+    {/* style={styles.background} */}
       <View>
-         <Text>LoginscreenProps</Text>
+         <Text>Caralho, quase não pega desgraçaaa</Text>
       </View>
+      </ImageBackground>
     );
 }
+
+// const styles = StyleSheet.create({
+//   background: {width: '100', height: '100%'}
+// });
