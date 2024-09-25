54c257fe6ba524b1d83960f13827b3616789dc8f
john665paes <johnatanpaes@gmail.com>
2024-09-25 12:21:15 -0300

    correção

--- licensync/App.tsx
+++ licensync/App.tsx
@@ -1,12 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import LoginScreen from './src/screens/login';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Salve cambada, bora pra frente!</Text>
-      <StatusBar style="auto" />
-    </View>
+  return (<LoginScreen/>
   );
 }
 
---
+++ licensync/src/screens/login/index.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+
+export interface LoginscreenProps {
+}
+
+export function App (props: LoginscreenProps) {
+    return (
+      <View>
+         <Text>LoginscreenProps</Text>
+      </View>
+    );
+}
